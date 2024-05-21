f1eb59b5256b716304d2e505ad3b7c156846d0f4
Jonathan Holck Lundin <eaajonhl@students.eaaa.dk>
2024-05-21 10:06:53 +0200

    Path

--- Shared/Paths.cs
+++ Shared/Paths.cs
@@ -3,9 +3,14 @@ namespace Core
 {
     public class Paths
     {
-        public static string FOLDER = @"/Users/jonat/Desktop/ItArkitekturPrincipper/TestData/Small";
+        //public static string FOLDER = @"/Users/jonat/Desktop/ItArkitekturPrincipper/TestData/Small";
 
-        public static string DATABASE = @"/Users/jonat/Desktop/ItArkitekturPrincipper/TestData/searchDBSmall.db";
+        //public static string DATABASE = @"/Users/jonat/Desktop/ItArkitekturPrincipper/TestData/searchDBSmall.db";
 
+
+
+        public static string FOLDER = @"/Users/Jonathan/OneDrive/Skrivebord/DataBaseItArkitekturPrincipper/small";
+
+        public static string DATABASE = @"/Users/Jonathan/OneDrive/Skrivebord/DataBaseItArkitekturPrincipper/SearchDBSmall.db";
     }
 }
