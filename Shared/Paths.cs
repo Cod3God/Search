2cf65d270c2915ed678735acfdb77aaf60890b28
Jonathan Holck Lundin <eaajonhl@students.eaaa.dk>
2024-05-27 12:52:51 +0200

    Initial NewSnippet

--- Shared/Paths.cs
+++ Shared/Paths.cs
@@ -3,14 +3,15 @@ namespace Core
 {
     public class Paths
     {
+        //Husk skift
 
         //Bærebar
         //public static string FOLDER = @"/Users/jonat/Desktop/ItArkitekturPrincipper/TestData/Small";
 
         //public static string DATABASE = @"/Users/jonat/Desktop/ItArkitekturPrincipper/TestData/searchDBSmall.db";
-        //Stationær
 
-        
+
+        //Stationær
         public static string FOLDER = @"/Users/Jonathan/OneDrive/Skrivebord/DataBaseItArkitekturPrincipper/small";
 
         public static string DATABASE = @"/Users/Jonathan/OneDrive/Skrivebord/DataBaseItArkitekturPrincipper/SearchDBSmall.db";
